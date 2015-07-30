3ca0f0407e2e62437fe72cef77af90509202bf47
jinnyeong <jin3216@naver.com>
2015-07-30 20:32:52 +0900

    Create LetterChanges.rb

---
+++ LetterChanges.rb
@@ -0,0 +1,20 @@
+def LetterChanges(str)
+
+
+  str = str.split("")
+    
+  str.each do |c|
+    c.next! if c =~ /[a-z]/
+    c.upcase! if c =~ /[aeiou]/
+  end
+  
+  
+  
+  # code goes here
+  return str.join
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LetterChanges(STDIN.gets)           
