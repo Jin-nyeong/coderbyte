7330dff1f858f4ba1d5eb513f0cfb2aa0b5b5556
jinnyeong <jin3216@naver.com>
2015-07-30 22:15:30 +0900

    Create CheckNums.rb

---
+++ CheckNums.rb
@@ -0,0 +1,15 @@
+def CheckNums(num1,num2)
+
+  num1 = gets.chomp
+  num2 = gets.chomp
+  
+  
+  if num2 > num1
+    puts "true"
+  elsif num2 <num1
+    puts "false"
+  else
+    puts "-1"
+  end
+  
+end
